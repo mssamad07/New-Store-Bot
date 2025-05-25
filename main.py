c2c6e3864c90b9922ad71b099aeb67c0ca7ec092
mssamad07 <ms.samad.0143@gmail.com>
2025-05-25 08:28:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from bot import bot
+from pyrogram import Client, filters
 
-if __name__ == "__main__":
-    bot.run()
+app = Client("my_bot")
+
+@app.on_message(filters.text)
+async def handle_text(client, message):
+    await message.reply("Text message received!")
+
+app.run()
